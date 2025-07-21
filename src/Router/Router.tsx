ceae25b165564984e119187f5c91b18e032e56d7
Tanuffka <tanyaa.ischenko@gmail.com>
2025-07-22 00:09:56 +0200

    [FB-9]: Create forgot password and reset password mockup

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -4,6 +4,8 @@ import DefaultLayout from 'src/layouts/DefaultLayout';
 import Home from 'src/pages/Home';
 import Login from 'src/pages/Login';
 import Register from 'src/pages/Register';
+import ResetPassword from 'src/pages/ResetPassword';
+import ForgotPassword from 'src/pages/ForgotPassword';
 
 export default function Router() {
   return (
@@ -12,6 +14,8 @@ export default function Router() {
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="reset-password" element={<ResetPassword />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/ResetPassword/ResetPassword.tsx
@@ -0,0 +1,115 @@
+import { Controller, FormProvider, useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
+import AuthLayout from 'src/layouts/AuthLayout';
+
+const loginSchema = z.object({
+  password: z.string().min(6, 'Password must be at least 6 characters'),
+  newPassword: z.string().min(6, 'Password must be at least 6 characters'),
+  confirmPassword: z.string().min(6, 'Password does not match'),
+});
+
+export default function ResetPassword() {
+  const form = useForm({
+    defaultValues: {
+      password: '',
+      newPassword: '',
+      confirmPassword: '',
+    },
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = form.handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <AuthLayout
+      title="Reset Password"
+      footerText="Already have an account?"
+      footerLink={{ to: '/login', content: 'Login' }}
+    >
+      <FormProvider {...form}>
+        <Box
+          noValidate
+          component="form"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '100%',
+            px: 6,
+          }}
+          autoComplete="off"
+          onSubmit={onSubmit}
+        >
+          <Controller
+            name="password"
+            control={form.control}
+            render={({ field, formState: { errors } }) => (
+              <TextField
+                fullWidth
+                required
+                label="Password"
+                variant="outlined"
+                error={!!errors.password}
+                helperText={errors.password?.message}
+                type="password"
+                sx={{ my: 1 }}
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            name="newPassword"
+            control={form.control}
+            render={({ field, formState: { errors } }) => (
+              <TextField
+                fullWidth
+                required
+                label="New password"
+                variant="outlined"
+                error={!!errors.newPassword}
+                helperText={errors.newPassword?.message}
+                type="password"
+                sx={{ my: 1 }}
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            name="confirmPassword"
+            control={form.control}
+            render={({ field, formState: { errors } }) => (
+              <TextField
+                fullWidth
+                required
+                label="Confirm password"
+                variant="outlined"
+                error={!!errors.confirmPassword}
+                helperText={errors.confirmPassword?.message}
+                type="password"
+                sx={{ my: 1 }}
+                {...field}
+              />
+            )}
+          />
+          <Button
+            fullWidth
+            type="submit"
+            variant="contained"
+            size="medium"
+            sx={{ mt: 1, mb: 2 }}
+          >
+            Reset
+          </Button>
+        </Box>
+      </FormProvider>
+    </AuthLayout>
+  );
+}
